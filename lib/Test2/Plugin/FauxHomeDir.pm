542e7a24b51019c66b13c0c96a2659bd50648236
Graham Ollis <plicease@cpan.org>
2017-07-22 17:28:19 -0400

    Initial commit

---
+++ lib/Test2/Plugin/FauxHomeDir.pm
@@ -0,0 +1,10 @@
+package Test2::Plugin::FauxHomeDir;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Setup a faux home directory for tests
+# VERSION
+
+1;
---
+++ t/test2_plugin_fauxhomedir.t
@@ -0,0 +1,6 @@
+use Test2::V0;
+use Test2::Plugin::FauxHomeDir;
+
+ok 1, 'todo';
+
+done_testing
