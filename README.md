3307d314bf6982a1f815edcf78a1770c055201d2
Graham Ollis <plicease@cpan.org>
2017-07-22 18:03:33 -0400

    initial try

---
+++ README.md
@@ -0,0 +1,14 @@
+# Test2::Plugin::FauxHomeDir [![Build Status](https://secure.travis-ci.org/plicease/Test2-Plugin-FauxHomeDir.png)](http://travis-ci.org/plicease/Test2-Plugin-FauxHomeDir)
+
+Setup a faux home directory for tests
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Test2/Plugin/FauxHomeDir.pm
+++ lib/Test2/Plugin/FauxHomeDir.pm
@@ -3,8 +3,57 @@ package Test2::Plugin::FauxHomeDir;
 use strict;
 use warnings;
 use 5.008001;
+use File::Temp qw( tempdir );
+use File::Spec;
+use if $^O eq 'cygwin', 'File::Spec::Win32';
 
 # ABSTRACT: Setup a faux home directory for tests
 # VERSION
+
+my $real;
+my $faux;
+my $user;
+
+sub import
+{
+  if($^O eq 'MSWin32')
+  {
+    $real = $ENV{USERPROFILE};
+    $real = File::Spec->catdir($ENV{HOMEDRIVE}, $ENV{HOMEPATH})
+      unless defined $real;
+    $user = $ENV{USERNAME};
+  }
+  else
+  {
+    $real = $ENV{HOME};
+    $user = $ENV{USER};
+  }
+  
+  die "unable to determine 'real' home directory"
+    unless defined $real && -d $real;
+  
+  delete $ENV{USERPROFILE};
+  delete $ENV{HOME};
+  delete $ENV{HOMEDRIVE};
+  delete $ENV{HOMEPATH};
+  
+  $faux = File::Spec->catdir(tempdir( CLEANUP => 1 ), 'home', $user);
+
+  if($^O eq 'MSWin32')
+  {
+    $ENV{USERPROFILE} = $faux;
+    ($ENV{HOMEDRIVE}, $ENV{HOMEPATH}) = File::Spec->splitpath($faux);
+  }
+  elsif($^O eq 'cygwin')
+  {
+    $ENV{USERPROFILE} = Cygwin::posix_to_win_path($faux);
+    ($ENV{HOMEDRIVE}, $ENV{HOMEPATH}) = File::Spec::Win32->splitpath($faux);
+    $ENV{HOME} = $faux;
+  }
+  else
+  {
+    $ENV{HOME} = $faux;
+  }
+}
 
 1;
